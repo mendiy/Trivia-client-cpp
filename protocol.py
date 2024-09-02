8d219d005e25758c8e9862fa2c5cc19b03898314
mendi <124784010+mendiy@users.noreply.github.com>
2024-09-02 16:28:34 +0300

    get msg

--- protocol.py
+++ protocol.py
@@ -5,25 +5,20 @@ LENGTH_FIELD_SIZE = 4
 PORT = 8000
 
 
-
 def create_msg(data, status=0):
     """Create a valid protocol message, with length field"""
     lenght = len(data)
     message = (status).to_bytes(1, byteorder='big') + len(data).to_bytes(4, byteorder='big') + data.encode("ASCII");
-    #print(message)
+    print(message)
     return message
 
 def get_msg(my_socket):
-    """Extract message from protocol, without the length field
-       If length field does not include a number, returns False, "Error" """
-    message_len = my_socket.recv(4).decode()
-
-    if str(message_len).isdigit() == False:
-        return False, "Error"
-    
-    message = my_socket.recv(int(message_len)).decode()
-
-    return True ,message
+	id = my_socket.recv(1)
+	length = int.from_bytes(my_socket.recv(4), byteorder='big')
+	message = ""
+	while len(message) < length:
+		message += (my_socket.recv(length)).decode("ASCII")
+	return id, message
 
 def login_msg():
     user_name = input("user name: ")
@@ -41,3 +36,4 @@ def signin_msg():
     data = json.dumps(user_data)
 
     return create_msg(data, 101)
+
